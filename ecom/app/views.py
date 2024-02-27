c1d16e48c49c855b11f487be7c7d23db3cd60761
mohammed-7 <khanhuzaif@live.com>
2024-02-27 11:34:31 +0530

    image static folder configerd

--- ecom/app/views.py
+++ ecom/app/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return render(request,"app/index.html")
+
+def navbar(request):
+    return render(request,"app/navbar.html")
+
+def navbar(request):
+    return render(request,"app/carousel.html")
