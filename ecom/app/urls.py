56e5542520022eebd71742053d6ee741ed1aab81
mohammed-7 <khanhuzaif@live.com>
2024-02-27 19:50:59 +0530

    commits before migrations

--- ecom/app/urls.py
+++ ecom/app/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('',views.index),
     path('',views.navbar),
-    
+    path('',views.carousel),
+    path('',views.cards),
     
 ]
--- ecom/app/views.py
+++ ecom/app/views.py
@@ -9,5 +9,8 @@ def index(request):
 def navbar(request):
     return render(request,"app/navbar.html")
 
-def navbar(request):
+def carousel(request):
     return render(request,"app/carousel.html")
+
+def cards(request):
+    return render(request,"app/cards.html")
