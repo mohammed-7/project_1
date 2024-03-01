4ced987a4dfec197d5791b45239bd2e8a6716d9f
mohammed-7 <khanhuzaif@live.com>
2024-03-01 18:29:12 +0530

    added media settings to settings.py and checked with urls.py of project .

--- ecom/app/models.py
+++ ecom/app/models.py
@@ -8,3 +8,10 @@ class Product(models.Model):
     product_name = models.CharField(max_length=50)
     desc = models.CharField(max_length=300)
     pub_date = models.DateField()
+    category = models.CharField(max_length=50,default="")
+    sub_category = models.CharField(max_length=50,default="")
+    price = models.IntegerField(default=0)
+    image = models.ImageField(upload_to="app/images",default="")
+    
+def __str__(self):
+    return self.product_name
